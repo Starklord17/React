a9ffd16784edafe8377719681cbde753582f8162
Max_S <91400948+Starklord17@users.noreply.github.com>
2022-08-17 00:37:47 -0300

    fetch
    How to consume an API

---
+++ API de Rick y Morty/index.js
@@ -0,0 +1,36 @@
+function getCharacters(done) {
+
+    const results = fetch("https://rickandmortyapi.com/api/character");
+
+    results.then(response => response.json())
+    .then(data => {
+        done(data)
+    })
+
+}
+
+getCharacters(data => {
+    console.log(data)
+
+    data.results.forEach(personaje => {
+
+        const article = document.createRange().createContextualFragment( /*html*/`
+        
+        <article>
+
+            <div class="image-container">
+                <img src="${personaje.image}" alt="Personaje">
+            </div>
+
+            <h2>${personaje.name}</h2>
+            <span>${personaje.status}</span>
+
+         </article>
+        `);
+
+        const main = document.querySelector("main");
+
+        main.append(article)
+
+    })
+});
